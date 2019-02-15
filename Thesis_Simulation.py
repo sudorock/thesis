9af921ac13123db3c8151bbf0e8a9b33a4c6360f
sunil <kslvsunil@gmail.com>
2019-02-15 20:32:15 +0100

    1. Rename file 2. Restructure code with variables at top

--- Thesis_Simulation.py
+++ Thesis_Simulation.py
@@ -3,88 +3,48 @@ import random
 import time
 import numpy as np
 
-dt = float(input("Enter a value for dt: "))
-v_initial = 1000
+dt = float(input("Enter a value for dt (0.01 for default): "))
 color_factor = float(input("Enter a value for Colour Change Determinancy (0 - 100): "))/100
+corner = int(input("Enter 1 to start in Corner and if otherwise 0: "))
+
+v_initial = 1000       # initial velocity of particle
 background_color = (255, 255, 255)
 (width, height) = (1300, 700)
-green = (0, 255, 0)
-red = (255, 0, 0)
 color1 = (255, 165, 80)  # orange
 color2 = (0, 0, 255)  # blue
 black = (0, 0, 0)
-corner = int(input("Enter 1 to start in Corner and if otherwise 0: "))
-thickness = 1
+thickness = 1   # thickness of particle
+particle_size = 10
+number_of_particles = 50
+particles = []
 
-table = np.zeros((1000, 10))  # table to store velocities
-t_iter = 0      # iterator for the table
+forward_table = np.zeros((1000, 10)) # table to store forward values
+# deterministic reverse velocity reverse_table
+reverse_table = np.zeros((1000, 10))  # reverse_table to store velocities
+t_iter = 0      # iterator for the reverse_table
 
-# store velocities before and after collision
+reverse = 0     # flag for reverse motion by reversing direction of velocities
+forward = 0     # flag for forward motion
+hist = 0        # flag for reverse motion from memory
+reverse_once = 0    # flag to stop reversing velocities every time it enters reverse motion for loop
+deterministic = 0  # use deterministic reverse_table for reversal
 
+collision_count = 0     # counter for number of collisions
 
-def store(p1, p2, t_iterator, before):
-    if before == 1:
-        # velocities before colllision
-        table[t_iterator, 2] = p1.v_x
-        table[t_iterator, 3] = p1.v_y
-        table[t_iterator, 4] = p2.v_x
-        table[t_iterator, 5] = p2.v_y
-        # Difference of positions
-        table[t_iterator, 0] = p2.x - p1.x
-        table[t_iterator, 1] = p2.y - p2.y
+history = {}    # store position and color in memory
+frame = 0   # counter for history
 
-    elif before == 0:
-        table[t_iterator, 6] = p1.v_x
-        table[t_iterator, 7] = p1.v_y
-        table[t_iterator, 8] = p2.v_x
-        table[t_iterator, 9] = p2.v_y
-        t_iterator = t_iterator + 1
+j = 0   # iterator for reverse frame
+start_time = 0  # store time when 'f' is pressed
+reverse_time = 0    # store time when 'r' is pressed
 
-    return t_iterator
+time_list = []  # stores time values in seconds
+r = 0   # iterator for time_list
+rr = 0  # flag to store index for time_list reversal when 'r' or 'h' is pressed
 
-
-def color_change(p1, p2):
-    if p2.color == color1 and p1.color == color1:
-        randomizer = random.random()
-        if randomizer <= color_factor:
-            p2.color = color2
-            p1.color = color2
-
-    elif p2.color == color2 and p1.color == color2:
-        randomizer = random.random()
-        if randomizer <= color_factor:
-            p2.color = color1
-            p1.color = color1
-
-
-def reverse_velocity(p):
-    p.v_x = -p.v_x
-    p.v_y = -p.v_y
-
-
-def check_collision(p1, p2):
-    s = (p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2  # s = square of distance between particles
-
-    if s < (p1.size + p2.size) ** 2:
-        return True
-
-
-def collide(p1, p2):
-    s = (p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2  # s = square of distance between particles
-    e1 = p2.x - p1.x
-    e2 = p2.y - p1.y
-    a = e1 * (p1.v_x - p2.v_x) + e2 * (p1.v_y - p2.v_y)  # numerator of lamda
-
-    if a >= 0:
-        lamda = a / s
-
-        # new velocities after collision
-        p1.v_x = p1.v_x - lamda * e1
-        p1.v_y = p1.v_y - lamda * e2
-        p2.v_x = p2.v_x + lamda * e1
-        p2.v_y = p2.v_y + lamda * e2
-
-    color_change(p1, p2)
+frame_rate = 0.03   # frame rate of display
+time_elapsed = 0    # to store time elapsed for clock
+display = 0     # flag for display of particles according to frame rate
 
 
 class Particle:
@@ -128,15 +88,71 @@ class Particle:
         self.y = self.y + self.v_y * dt
 
 
-pygame.init()
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Direction of Time')
-screen.fill(background_color)
-font = pygame.font.SysFont(None, 22)
+def collide(p1, p2):
+    s = (p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2  # s = square of distance between particles
+    e1 = p2.x - p1.x
+    e2 = p2.y - p1.y
+    a = e1 * (p1.v_x - p2.v_x) + e2 * (p1.v_y - p2.v_y)  # numerator of lamda
 
-particle_size = 10
-number_of_particles = 50
-particles = []
+    if a >= 0:
+        lamda = a / s
+
+        # new velocities after collision
+        p1.v_x = p1.v_x - lamda * e1
+        p1.v_y = p1.v_y - lamda * e2
+        p2.v_x = p2.v_x + lamda * e1
+        p2.v_y = p2.v_y + lamda * e2
+
+    color_change(p1, p2)
+
+
+def color_change(p1, p2):
+    if p2.color == color1 and p1.color == color1:
+        randomizer = random.random()
+        if randomizer <= color_factor:
+            p2.color = color2
+            p1.color = color2
+
+    elif p2.color == color2 and p1.color == color2:
+        randomizer = random.random()
+        if randomizer <= color_factor:
+            p2.color = color1
+            p1.color = color1
+
+
+def reverse_velocity(p):
+    p.v_x = -p.v_x
+    p.v_y = -p.v_y
+
+
+def check_collision(p1, p2):
+    s = (p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2  # s = square of distance between particles
+
+    if s < (p1.size + p2.size) ** 2:
+        return True
+
+
+# store velocities before and after collision
+def store(p1, p2, t_iterator, before):
+    if before == 1:
+        # velocities before colllision
+        reverse_table[t_iterator, 2] = int(p1.v_x)
+        reverse_table[t_iterator, 3] = int(p1.v_y)
+        reverse_table[t_iterator, 4] = int(p2.v_x)
+        reverse_table[t_iterator, 5] = int(p2.v_y)
+        # Difference of positions
+        reverse_table[t_iterator, 0] = int(abs(p2.x - p1.x))
+        reverse_table[t_iterator, 1] = int(abs(p2.y - p1.y))
+
+    elif before == 0:
+        reverse_table[t_iterator, 6] = int(p1.v_x)
+        reverse_table[t_iterator, 7] = int(p1.v_y)
+        reverse_table[t_iterator, 8] = int(p2.v_x)
+        reverse_table[t_iterator, 9] = int(p2.v_y)
+        t_iterator = t_iterator + 1
+
+    return t_iterator
+
 
 # initialize particles
 
@@ -162,29 +178,13 @@ else:
         particle = Particle(x_init, y_init, v_x_init, v_y_init, particle_size, color1)
         particles.append(particle)
 
+
 running = True
-
-reverse = 0
-forward = 0
-hist = 0
-reverse_once = 0    # flag to stop reversing velocities every time it enters reverse motion for loop
-
-collision_count = 0
-
-history = {}    # store position and color in memory
-frame = 0   # counter for history
-
-j = 0   # iterator for reverse frame
-start_time = 0  # store time when 'f' is pressed
-reverse_time = 0    # store time when 'r' is pressed
-
-time_list = []  # stores time values in seconds
-r = 0   # iterator for time_list
-rr = 0  # flag to store index for time_list reversal when 'r' or 'h' is pressed
-
-frame_rate = 0.03
-time_elapsed = 0
-display = 0
+pygame.init()
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Direction of Time')
+screen.fill(background_color)
+font = pygame.font.SysFont(None, 22)
 
 while running:
     for event in pygame.event.get():
@@ -207,18 +207,30 @@ while running:
             hist = 1
             j = frame - 1
             rr = r - 1
+            reverse_time = time.time()
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_d:    # press 'h' for reversing motion from memory
+            reverse = 0
+            forward = 0
+            hist = 0
+            reverse_once = 0
+            deterministic = 1
+            rr = r - 1
+            reverse_time = time.time()
 
     # refresh screen with background for every frame
-    if time.time() - time_elapsed >= frame_rate and time.time() - time_elapsed <= frame_rate + 1:
-        screen.fill(background_color)
-        display = 1
-        time_elapsed = time.time()
+    if time.time() - time_elapsed >= frame_rate:
+        if time.time() - time_elapsed <= frame_rate + 1:
+            screen.fill(background_color)
+            display = 1
+            time_elapsed = time.time()
+        else:
+            display = 0
     else:
         display = 0
 
     # Initial display of particles
 
-    if forward == 0 and reverse == 0 and hist == 0:
+    if forward == 0 and reverse == 0 and hist == 0 and deterministic == 0:
         for particle in particles:
             particle.display()
 
@@ -299,6 +311,42 @@ while running:
         screen.blit(reverse_timer, (width - 80, 20))
         screen.blit(font.render("Time: " + str(time_list[rr]), True, black, background_color), (width - 150, 20))
 
+# deterministic reverse with reverse_table
+    if forward == 0 and reverse == 0 and hist == 0 and deterministic == 1:
+        r = r - 1
+        # stop animation when reverse time reaches 0
+        if (time.time() - reverse_time == reverse_time - start_time) or r == -1:
+            pygame.time.wait(10000)
+            running = False
+        if reverse_once == 0:
+            for particle in particles:
+                reverse_velocity(particle)
+                reverse_once = 1
+        for i, particle in enumerate(particles):
+            particle.move()
+            particle.wall_bounce()
+            for particle2 in particles[i + 1:]:
+                collision_check = [int(abs(particle2.x - particle.x)), int(abs(particle2.y - particle.y)), int(particle.v_x), int(particle.v_y), int(particle2.v_x), int(particle2.v_y)]
+                for k in range(0, t_iter):      # compare with reverse_table and change velocities
+                    table_check1 = [reverse_table[k, 0], reverse_table[k, 1], reverse_table[k, 6], reverse_table[k, 7], reverse_table[k, 8], reverse_table[k, 9]]
+                    table_check2 = [reverse_table[k, 0], reverse_table[k, 1], reverse_table[k, 2], reverse_table[k, 3], reverse_table[k, 4], reverse_table[k, 5]]
+                    if np.array_equal(collision_check, table_check1):   # if there is a corresponding row for collision with v1 and v2, then change to w1 and w2
+                        particle.v_x = reverse_table[k, 2]
+                        particle.v_y = reverse_table[k, 3]
+                        particle2.v_x = reverse_table[k, 4]
+                        particle2.v_y = reverse_table[k, 5]
+                    elif np.array_equal(collision_check, table_check2): # if there is a corresponding row for collision with w1 and w2, then change to v1 and v2
+                        particle.v_x = reverse_table[k, 6]
+                        particle.v_y = reverse_table[k, 7]
+                        particle2.v_x = reverse_table[k, 8]
+                        particle2.v_y = reverse_table[k, 9]
+            if display == 1:
+                particle.display()
+
+        reverse_timer = font.render(str(time_list[r]), True, red, background_color)
+        screen.blit(reverse_timer, (width - 80, 20))
+        screen.blit(font.render("Time: " + str(time_list[rr]), True, black, background_color), (width - 150, 20))
+
     if hist == 0:
         frame = frame + 1
     elif hist == 1:
@@ -306,4 +354,23 @@ while running:
 
     pygame.display.flip()
 
-np.savetxt('tabledump.txt', table, fmt='%1.0f', delimiter=',')
+
+# check for duplicate rows in stored reverse_table
+delete_rows = []
+k = 0
+
+for i in range(0, len(reverse_table)):
+    for j in range(0, i):
+        if np.array_equal(reverse_table[i],reverse_table[j]) and reverse_table[i].all():
+            delete_rows[k] = j
+            k += 1
+
+# delete duplicate rows
+for i in range(0, k):
+    reverse_table = np.delete(reverse_table, i, axis=0)
+
+# save reverse_table as .txt
+np.savetxt('tabledump.txt', reverse_table, fmt='%d', delimiter=',')
+
+
+#'%1.0f'
