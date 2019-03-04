d380d5a77e1ad1785cd6ca8a935f34709ca4202b
sunil <kslvsunil@gmail.com>
2019-03-05 00:55:17 +0100

    still checking why it doesn't reverse to inital position in all cases. Committing before cleaning code

--- Test/reverse_table_creator.py
+++ Test/reverse_table_creator.py
@@ -9,18 +9,17 @@ corner = 1  # int(input("Enter 1 to start in Corner and if otherwise 0: "))
 
 v_initial = float(input("Enter an initial value for velocity: "))  # initial velocity of particle
 background_color = (255, 255, 255)
-(width, height) = (1000, 700)
+(width, height) = (500, 500)
 red = (255, 0, 0)
 green = (0, 255, 0)
 color1 = (255, 165, 80)  # orange
 color2 = (0, 0, 255)  # blue
 black = (0, 0, 0)
 thickness = 1  # thickness of particle
-particle_size = 10
-number_of_particles = 20
+particle_size = 20
+number_of_particles = 6
 particles = []
 
-forward_table = np.zeros((1000, 10))  # table to store forward values
 # deterministic reverse velocity reverse_table
 reverse_table = np.zeros((10000, 10))  # reverse_table to store velocities
 t_iter = 0  # iterator for the reverse_table
@@ -94,19 +93,21 @@ class Particle:
             self.v_y = -self.v_y  # reverse velocity y-direction
 
     def move(self):
-        print("Position and Velocity  x: ", self.x, " y: ", self.y, " v_x: ", self.v_x, " v_y: ", self.v_y)
+        # print("Position and Velocity  x: ", self.x, " y: ", self.y, " v_x: ", self.v_x, " v_y: ", self.v_y)
         self.x = self.x + self.v_x * dt
         self.y = self.y + self.v_y * dt
-
 
 
 # check if the particles collided
 def check_collision(p1, p2):
     s = (p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2  # s = square of distance between particles
 
-    if s < (p1.size + p2.size) ** 2:
-        print("Check Collision s: ", s, " Rx: ", (p1.x - p2.x), " Ry: ", (p1.y - p2.y))
+    if s <= (p1.size + p2.size) ** 2 and s != 0:
+        # print("Check Collision s: ", s, " Rx: ", (p1.x - p2.x), " Ry: ", (p1.y - p2.y))
         return True
+    else:
+        return False
+
 
 
 # particle collision
@@ -116,10 +117,10 @@ def collide(p1, p2):
     e2 = p2.y - p1.y
     a = e1 * (p1.v_x - p2.v_x) + e2 * (p1.v_y - p2.v_y)  # numerator of lamda
 
-    if a >= 0 and s != 0:
+    if a >= 0:
         lamda = a / s
-        print("Check Collision inside lambda s: ", s, " Rx: ", (p1.x - p2.x), " Ry: ", (p1.y - p2.y))
-        print("\n Before Lambda Rx: ", p1.x - p2.x, " Ry: ", p1.y - p2.y, " v1_x: ", p1.v_x, " v1_y: ", p1.v_y, " v2_x: ", p2.v_x, " v2_y: ", p2.v_y)
+        # print("Check Collision inside lambda s: ", s, " Rx: ", (p1.x - p2.x), " Ry: ", (p1.y - p2.y))
+        # print("\n Before Lambda Rx: ", p1.x - p2.x, " Ry: ", p1.y - p2.y, " v1_x: ", p1.v_x, " v1_y: ", p1.v_y, " v2_x: ", p2.v_x, " v2_y: ", p2.v_y)
         # new velocities after collision
         p1.v_x = int(p1.v_x - lamda * e1)
         p1.v_y = int(p1.v_y - lamda * e2)
@@ -129,7 +130,7 @@ def collide(p1, p2):
         # p1.v_y = p1.v_y - lamda * e2
         # p2.v_x = p2.v_x + lamda * e1
         # p2.v_y = p2.v_y + lamda * e2
-        print("\n After Lambda Rx: ", p1.x - p2.x, " Ry: ", p1.y - p2.y, " v1_x: ", p1.v_x, " v1_y: ", p1.v_y, " v2_x: ", p2.v_x, " v2_y: ", p2.v_y)
+        # print("\n After Lambda Rx: ", p1.x - p2.x, " Ry: ", p1.y - p2.y, " v1_x: ", p1.v_x, " v1_y: ", p1.v_y, " v2_x: ", p2.v_x, " v2_y: ", p2.v_y)
     color_change(p1, p2)
 
 
@@ -152,7 +153,7 @@ def color_change(p1, p2):
 def reverse_velocity(p):
     p.v_x = -p.v_x
     p.v_y = -p.v_y
-    print("Reverse Velocity point: x: ", p.x, " y: ", p.y, " v_x: ", p.v_x, " v_y: ", p.v_y)
+    # print("Reverse Velocity point: x: ", p.x, " y: ", p.y, " v_x: ", p.v_x, " v_y: ", p.v_y)
 
 
 # store velocities before and after collision
@@ -166,8 +167,13 @@ def store(p1, p2, t_iterator, before):
         reverse_table[t_iterator, 4] = -int(p2.v_x)
         reverse_table[t_iterator, 5] = -int(p2.v_y)
         # Difference of positions
-        reverse_table[t_iterator, 0] = int(abs(p2.x - p1.x))
-        reverse_table[t_iterator, 1] = int(abs(p2.y - p1.y))
+        # reverse_table[t_iterator, 0] = int(abs(p2.x - p1.x))
+        # reverse_table[t_iterator, 1] = int(abs(p2.y - p1.y))
+        # reverse_table[t_iterator, 0] = abs(p2.x - p1.x)
+        # reverse_table[t_iterator, 1] = abs(p2.y - p1.y)
+        reverse_table[t_iterator, 0] = p2.x - p1.x
+        reverse_table[t_iterator, 1] = p2.y - p1.y
+
 
     elif before == 0:
         reverse_table[t_iterator, 6] = -int(p1.v_x)
@@ -200,18 +206,24 @@ def display_time(fwd, counter):
 pos = {}
 c_count = 0
 c_count2 = 0
+disp_once = 0
+total_collision_count = 0
 # initialize particles
 if corner == 1:
     j = 1
     k = 1
     while j <= number_of_particles:
         for z in range(1, int(number_of_particles ** 0.5) + 1):
-            x_init = 4 * z * particle_size + particle_size * int((random.random() - 0.5))
-            y_init = 4 * k * particle_size + particle_size * int((random.random() - 0.5))
-            v_x_init = v_initial * random.randint(0,5)
-            v_y_init = v_initial * random.randint(0,5)
+            x_init = 4 * z * particle_size + particle_size  #* int((random.random() - 0.5))
+            y_init = 4 * k * particle_size + particle_size  #* int((random.random() - 0.5))
+            # x_init = z * particle_size + particle_size  # * int((random.random() - 0.5))
+            # y_init = 1.5*k * particle_size + particle_size
+            # v_x_init = v_initial
+            # v_y_init = 0
+            v_x_init = v_initial * random.randint(0, 3)
+            v_y_init = v_initial * random.randint(0, 7)
             pos[j] = (x_init, y_init)
-            print("Initial Position: ", j, " ", pos[j])
+            # print("Initial Position: ", j, " ", pos[j])
             particle = Particle(x_init, y_init, v_x_init, v_y_init, particle_size, color1)
             particles.append(particle)
             j = j + 1
@@ -240,7 +252,7 @@ while running:
             reverse_once = 0
             rr = timer_counter - 1  # index at which time is reversed
             reverse_time = time.time()
-            print("Velocity reversed: \n \n")
+            # print("Velocity reversed: \n \n")
         if event.type == pygame.KEYDOWN and event.key == pygame.K_h:  # press 'h' for reversing motion from memory
             reverse = 0
             forward = 0
@@ -248,7 +260,7 @@ while running:
             rev_frame = frame - 1
             rr = timer_counter - 1  # index at which time is reversed
             reverse_time = time.time()
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_d:  # press 'h' for reversing motion from memory
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_d:  # press 'd' for reversing motion from table
             reverse = 0
             forward = 0
             hist = 0
@@ -329,6 +341,7 @@ while running:
             for particle in particles:
                 reverse_velocity(particle)
                 reverse_once = 1
+
         for i, particle in enumerate(particles):
             for particle2 in particles[i + 1:]:
                 if check_collision(particle, particle2):
@@ -372,9 +385,9 @@ while running:
 
         # time.time() - reverse_time == reverse_time - start_time) or
         if timer_counter < 0:
-            for particle in particles:
-                print("Final Position: ", particle.x, " ", particle.y)
-            pygame.time.wait(10000)
+            # for particle in particles:
+                # print("Final Position: ", particle.x, " ", particle.y)
+            pygame.time.wait(3000)
             running = False
             break
 
@@ -386,30 +399,58 @@ while running:
         for i, particle in enumerate(particles):
             for particle2 in particles[i + 1:]:
                 if check_collision(particle, particle2):
-                    # collision_check = [int(particle.v_x), int(particle.v_y), int(particle2.v_x), int(particle2.v_y)]
-                    collision_check = [int(abs(particle2.x - particle.x)), int(abs(particle2.y - particle.y)),
-                                       int(particle.v_x), int(particle.v_y), int(particle2.v_x), int(particle2.v_y)]
                     # compare with reverse_table and change velocities
-                    c_count += 1
-                    for k in range(0, t_iter):
-                        # table_check1 = [reverse_table[k, 6], reverse_table[k, 7],
-                        #                 reverse_table[k, 8], reverse_table[k, 9]]
-                        # table_check2 = [reverse_table[k, 2], reverse_table[k, 3],
-                        #                 reverse_table[k, 4], reverse_table[k, 5]]
-                        table_check1 = [reverse_table[k, 0], reverse_table[k, 1], reverse_table[k, 6], reverse_table[k, 7],
-                                        reverse_table[k, 8], reverse_table[k, 9]]
-                        table_check2 = [reverse_table[k, 0], reverse_table[k, 1], reverse_table[k, 2], reverse_table[k, 3],
-                                        reverse_table[k, 4], reverse_table[k, 5]]
-                        # print(collision_check, " ", table_check1, " ", table_check2)
-                        # if there is a corresponding row for collision with v1 and v2, then change to w1 and w2
-                        if np.array_equal(collision_check, table_check1):
-                            particle.v_x = reverse_table[k, 2]
-                            particle.v_y = reverse_table[k, 3]
-                            particle2.v_x = reverse_table[k, 4]
-                            particle2.v_y = reverse_table[k, 5]
-                            color_change(particle, particle2)
-                            c_count2 += 1
-                            print("Collision found 1:  ", "Actual data: ", collision_check, "Table data: ", table_check1, "reversed to:", table_check2)
+                    collision_count = collision_count + 1
+                    colliding_particle = particle2
+            if collision_count == 1:
+                # collision_check = [int(particle.v_x), int(particle.v_y), int(particle2.v_x), int(particle2.v_y)]
+                collision_check = [colliding_particle.x - particle.x,
+                                   colliding_particle.y - particle.y,
+                                   particle.v_x, particle.v_y, colliding_particle.v_x,
+                                   colliding_particle.v_y]
+                # collision_check = [int(abs(colliding_particle.x - particle.x)), int(abs(colliding_particle.y - particle.y)),
+                #                    int(particle.v_x), int(particle.v_y), int(colliding_particle.v_x), int(colliding_particle.v_y)]
+                c_count += 1
+
+                for k in range(0, t_iter):
+                    # table_check1 = [reverse_table[k, 6], reverse_table[k, 7],
+                    #                 reverse_table[k, 8], reverse_table[k, 9]]
+                    # table_check2 = [reverse_table[k, 2], reverse_table[k, 3],
+                    #                 reverse_table[k, 4], reverse_table[k, 5]]
+                    table_check1 = [reverse_table[k, 0], reverse_table[k, 1], reverse_table[k, 6], reverse_table[k, 7],
+                                    reverse_table[k, 8], reverse_table[k, 9]]
+                    table_check2 = [reverse_table[k, 0], reverse_table[k, 1], reverse_table[k, 2], reverse_table[k, 3],
+                                    reverse_table[k, 4], reverse_table[k, 5]]
+                    # print(collision_check, " ", table_check1, " ", table_check2)
+                    # if there is a corresponding row for collision with v1 and v2, then change to w1 and w2
+                    if np.array_equal(collision_check, table_check1):
+                        particle.v_x = reverse_table[k, 2]
+                        particle.v_y = reverse_table[k, 3]
+                        colliding_particle.v_x = reverse_table[k, 4]
+                        colliding_particle.v_y = reverse_table[k, 5]
+                        color_change(particle, colliding_particle)
+                        c_count2 += 1
+                        print("Collision found 1:  ", "Actual data: ", collision_check, "Table data: ", table_check1,
+                              "reversed to:", table_check2)
+                        break
+
+                if disp_once != c_count - c_count2:
+                    # screen.fill(background_color)
+                    # colliding_particle.display()
+                    # particle.display()
+                    # pygame.time.wait(5000)
+                    print("c_count - c_count2:",c_count - c_count2, "c_count:", c_count, "c_count2:", c_count2,
+                          " Collision not found in table for: ", collision_check)
+                    disp_once = c_count - c_count2
+                    # screen.fill(background_color)
+                    # particle.display()
+                    # particle2.display()
+                    # pygame.time.wait(1000)
+                    # break
+            elif collision_count > 1:
+                print("------Multiple collision:", collision_count)
+            collision_count = 0
+
 
                         # if there is a corresponding row for collision with w1 and w2, then change to v1 and v2
                         # elif np.array_equal(collision_check, table_check2):
@@ -447,6 +488,9 @@ while running:
 
     pygame.display.flip()
 
+
+print("Actual Collisions: ",c_count, " Collisions found in table: ", c_count2)
+
 # #check for duplicate rows in stored reverse_table
 delete_rows = []
 
@@ -481,7 +525,7 @@ reverse_table = np.delete(reverse_table, delete_rows, axis=0)
 #                 reverse_table[k, 4] = reverse_table[k, 4] - 1
 #                 reverse_table[k, 5] = reverse_table[k, 5] + 1
 
-print(c_count, " ", c_count2)
+
 # save reverse_table as .txt
 np.savetxt('tabledump.txt', reverse_table, fmt='%d', delimiter=',')
 
@@ -491,12 +535,7 @@ np.savetxt('tabledump.txt', reverse_table, fmt='%d', delimiter=',')
 # for i in range(0, len(delete_rows)):
 #     reverse_table = np.delete(reverse_table, delete_rows[i], axis=0)
 
-
-# 1. was never able to match a row
-# 2. store forward motion in memory
-# 3. reverse motion using deterministic table and store motion in memory
-
-# 4. w1 and w2 table entry must be -w1 and -w2
-# 5. Int value of dt and all other values
+# reversal during collision
+# sticking particles
 
 # loadtxt
